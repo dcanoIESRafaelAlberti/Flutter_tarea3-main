de78b19c508e95dc0eaa00dce74414f6a1cb3bb5
dcsibon <dcsibon@gmail.com>
2024-06-05 11:04:43 +0200

    Versión finalizada de la tarea 3 y los ejemplos

--- lib/app/view/result_page.dart
+++ lib/app/view/result_page.dart
@@ -3,30 +3,49 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_tarea3/app/view/util_views.dart';
 
-class ResultPage extends StatelessWidget {
-  ResultPage({super.key});
+class ResultPage extends StatefulWidget {
+  const ResultPage({super.key});
 
+  @override
+  State<ResultPage> createState() => _ResultPageState();
+}
+
+class _ResultPageState extends State<ResultPage> {
   final List<String> gamePlay = ["rock", "paper", "scissors"];
+  int player1Play = 0;
+  int player2Play = 0;
 
   @override
   Widget build(BuildContext context) {
-    //Random plays
-    int player1Play = Random().nextInt(3);
-    int player2Play = Random().nextInt(3);
-
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           const HeaderView("Well played!!"),
-          const SizedBox(height: 25,),
+          const SizedBox(
+            height: 25,
+          ),
           BodyView(gamePlay[player1Play], gamePlay[player2Play]),
           playerWinner(gamePlay[player1Play], gamePlay[player2Play]),
-          Button("BACK", () => Navigator.of(context).pop()),
+          Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Button("BACK", () => Navigator.of(context).pop()),
+                const SizedBox(width: 20),
+                Button("PLAY AGAIN", () => playAgain()),
+              ]
+          ),
           const BottomView(),
         ],
       ),
     );
+  }
+
+  void playAgain() {
+    return setState(() {
+                    player1Play = Random().nextInt(3);
+                    player2Play = Random().nextInt(3);
+                  });
   }
 
   Widget playerWinner(String player1, String player2) {
@@ -42,16 +61,13 @@ class ResultPage extends StatelessWidget {
     }
 
     return Container(
-          height: 96,
-          alignment: Alignment.center,
-          child: Text(
-            result,
-            style: const TextStyle(
-                fontFamily: "Poppins",
-                fontSize: 20,
-                fontWeight: FontWeight.w600),
-          ),
-        );
+      height: 96,
+      alignment: Alignment.center,
+      child: Text(
+        result,
+        style: const TextStyle(
+            fontFamily: "Poppins", fontSize: 20, fontWeight: FontWeight.w600),
+      ),
+    );
   }
 }
-
